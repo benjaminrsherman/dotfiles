a35ebfb8e63ad04516386e5ca50edcebe4373690
Benjamin Sherman <benjamin@bensherman.io>
2020-02-12 13:58:47 +0000

    Update fish usage

--- install.sh
+++ install.sh
@@ -6,6 +6,3 @@ cp gitconfig "$HOME/.gitconfig"
 # set up vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# set up fish
-fish setup_fish.fish
