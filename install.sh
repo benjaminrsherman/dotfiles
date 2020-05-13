20e893da6b9ed4261c858b4bb51c8eb36d9bad7e
Benjamin Sherman <Benjamin.Sherman@leidos.com>
2020-05-13 16:17:18 -0400

    Add leader keys to vim

--- install.sh
+++ install.sh
@@ -2,10 +2,8 @@
 
 # set up symlinks
 for f in config/*; do
-	ln -s $f $HOME/.config
+	ln $(pwd)/config/$f $HOME/.config
 done
-
-ln -s spacemacs $HOME/.spacemacs
 
 # set up vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
