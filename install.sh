8f53ddafdc4980a7d07a6a7efd4bb77d39cf752d
Benjamin Sherman <Benjamin.Sherman@leidos.com>
2020-07-03 15:28:54 -0400

    Add directory for binaries and scripts to be added to the apth

--- install.sh
+++ install.sh
@@ -5,6 +5,11 @@ for f in config/*; do
 	ln -s $(pwd)/$f $HOME/.config
 done
 
+# set up symlinks
+for f in local/*; do
+	ln -s $(pwd)/$f $HOME/.local
+done
+
 # set up vim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
