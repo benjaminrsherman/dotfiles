f7c267a5c0f5f846afe33fa57fc6162e66eba60b
Benjamin Sherman <benjamin@bensherman.io>
2022-02-12 17:57:48 -0500

    Update prompt to be C:\\

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,5 @@
 function fish_prompt
-	set_color $fish_color_cwd
-	echo -n -s (hostname)': '(basename (prompt_pwd))' $ '
+    set_color $fish_color_cwd
+    #echo -n -s (hostname)': '(basename (prompt_pwd))' $ '
+    echo -n -s 'C:\\'(pwd | sed 's/\//\\\/g')' $ '
 end
