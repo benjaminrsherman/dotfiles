950e96b4d0cfc1d237bd2436bb1e390826c81bc1
Benjamin Sherman <benjamin@bensherman.io>
2022-02-12 17:58:17 -0500

    Add filter to todo command

--- config/fish/functions/todo.fish
+++ config/fish/functions/todo.fish
@@ -1,4 +1,7 @@
-function todo
+function todo -a filter
     todoist sync
-    script -q -c "todoist --color l -f '#Inbox'" /dev/null | cut -f 1,2 -d ' ' --complement
+    if test -z "$filter"
+        set filter today
+    end
+    script -q -c "todoist --color l -f '$filter'" /dev/null | cut -f 1,2,3,4 -d ' ' --complement
 end
